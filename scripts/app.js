5f83d26558bddb03145f96590fa7d7bf9f763330
Adam Kahirov <uselessinformationzs@gmail.com>
2023-02-23 12:19:42 +0100

    sciherheti

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,8 @@ function checkPasswort() {
 	const inputBox = document.getElementById("pwbox");
 	const enteredpassword = inputBox.value;
 
-	const correctpassword = "NeoxTron321";
-	const correctpassword2 = "KimiBimi321";
+	const correctpassword = "NeonTest";
+	const correctpassword2 = "KimiTest";
 
 	if(enteredpassword===correctpassword || enteredpassword===correctpassword2) {
 		console.log("Richtiges Passwort!");
