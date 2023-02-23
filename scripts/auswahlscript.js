b193bb9a532deafee7f120833a29fc42143143e3
zkurs4np <uselessinformationzs@gmail.com>
2023-02-23 22:32:47 +0100

    posi

--- scripts/auswahlscript.js
+++ scripts/auswahlscript.js
@@ -1,9 +1,6 @@
 function aendern(myElement) {
-	// Speichern des Titels in eine Variable
-	// Verändern des Inhalts von h3 "Hallo" in den gespeicherten Titel
-
-	//Erfassen des aufrufenden Elements
 	let tag = document.getElementById("veraenderbar");
 	let kopierinhalt = myElement.innerHTML;
 	tag.innerHTML = kopierinhalt;
 }
+
