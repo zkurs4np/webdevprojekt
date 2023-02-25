c1c83cd1306aa5a62770cd15ad51ad49adaca5de
zkurs4np <adamkahirov@web.de>
2023-02-25 20:31:10 +0100

    togglediv start

--- scripts/auswahlscript.js
+++ scripts/auswahlscript.js
@@ -4,3 +4,26 @@ function aendern(myElement) {
 	tag.innerHTML = kopierinhalt;
 }
 
+function togglediv(toggledDiv) {
+	let activediv = null;
+	let newdiv = toggledDiv.innerHTML.toLowerCase();
+	if(document.getElementsByClassName("e-mails")[0].style.display !== "none")
+		{
+			activediv = document.getElementsByClassName("e-mails");
+			alert("Email")
+		}
+	else if(document.getElementByClass("kalender")[0].style.display !== "none")
+		{
+			activediv = document.getElementsByClassName("kalender");
+		}
+	else if(document.getElementByClass("organigramm")[0].style.display !== "none")
+		{
+			activediv = document.getElementsByClassName("organigramm");
+		}
+	else if(document.getElementByClass("taskliste")[0].style.display !== "none")
+		{
+			activediv = document.getElementsByClassName("taskliste");
+		}
+	activediv.display = "none";
+	newdiv.display = "block";
+}
